65d984577718c0955b3d9e512565b4d9f535e830
Morii Shikaa <ramanavami32@gmail.com>
2021-04-08 17:29:52 +0800

    trying to fix the input tag

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   }, []);
   return (
     <main>
-      <input type="file" name="mantap"></input>
+      <input type="file" name="mantap" />
       <h1>Create React App + Go API</h1>
       <h2>
         Deployed with{' '}
